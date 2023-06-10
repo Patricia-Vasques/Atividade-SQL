b09e63b2c9b078ed9335e9caffd2c23adbf331f6
Patrícia Vasques <patriciahhhh@gmail.com>
2023-06-10 13:22:06 -0300

    Criando nova chave estrangeira

---
+++ Atividade-5.sql
@@ -0,0 +1,35 @@
+create table Projeto (
+ id SERIAL primary key,	
+ nome VARCHAR (80),
+ descrição VARCHAR (400),
+ data_inicio DATE,
+ data_prazo DATE
+);
+
+
+create table Tarefa(
+ id SERIAL primary key,
+ nome VARCHAR (80),
+ descrição VARCHAR (400),
+ data_inicio DATE,
+ data_prazo DATE,
+ concluida BOOLEAN
+ );
+
+create table Categoria(
+	id SERIAL primary key,
+	nome VARCHAR (50),
+	descricao VARCHAR (200)
+);
+
+ALTER TABLE Tarefa
+ADD COLUMN categoria_id INT;
+
+ALTER TABLE Tarefa
+ADD COLUMN projeto_id INT;
+
+
+ALTER TABLE Tarefa
+ADD CONSTRAINT fk_categoria
+FOREIGN KEY (categoria_id, projeto_id) REFERENCES Categoria(id), Projeto(id);
+
