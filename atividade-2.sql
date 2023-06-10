7b1a81b030de3f9f76d02490cb0c24d8614da37d
Patrícia Vasques <patriciahhhh@gmail.com>
2023-06-10 12:03:42 -0300

    criação da tabela de categorias

--- atividade-2.sql
+++ atividade-2.sql
@@ -1,17 +1,5 @@
-create table Projeto (
+create table Categoria(
 	id SERIAL primary key,
-	nome VARCHAR (80),
-	descricao VARCHAR (400),
-	data_inicio DATE,
-	data_prazo DATE
-);
-
-
-create table Tarefa(
-	id SERIAL primary key,
-	nome VARCHAR (80),
-	descricao VARCHAR (400),
-	data_inicio DATE,
-	data_prazo DATE,
-	concluida integer
+	nome VARCHAR (50),
+	descricao VARCHAR (200)
 );
